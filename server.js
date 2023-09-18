937a788f3fbf718ea4a4f9170de3c70e7d74108d
Brandon <bjpaul99@gmail.com>
2023-09-18 15:04:58 -0600

    changed domain name to be salvusai.com

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/salvusfrontend'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname+
+//     '/dist/salvusfrontend/index.html'));});
+// app.listen(process.env.PORT || 8080);
+//
+// const cors = require('cors');
+// app.use(cors());
+
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/salvusfrontend'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/salvusfrontend/index.html'));});
-app.listen(process.env.PORT || 8080);
-
 const cors = require('cors');
+
+const app = express();
+
 app.use(cors());
+app.use(express.static(__dirname + '/dist/salvusfrontend'));
+
+// Redirect to salvusai.com
+app.get('/*', function(req, res) {
+  res.redirect(301, 'https://salvusai.com' + req.originalUrl);
+});
+
+app.listen(process.env.PORT || 8080);
